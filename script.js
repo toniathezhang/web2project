5d175d1e7150572e3802f17a5d8000cd555d7072
NYUAD <nyuad@C2-LIB-MBP-19.local>
2021-02-16 16:18:38 +0400

    forgot to comment lol. nwo pushing with comments

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ function play() {
     var audio = document.getElementById("audio");
     audio.play();
   }
+
+// code from here: https://stackoverflow.com/questions/18826147/javascript-audio-play-on-click.
